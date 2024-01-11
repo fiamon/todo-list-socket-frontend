f1169bcff06aeacedc54084ac98ca459b0ee4268
fiamon <juliogustavofi@gmail.com>
2024-01-11 16:00:29 -0300

    feat: form to create a board

---
+++ scripts/boards/createBoard.js
@@ -0,0 +1,42 @@
+const createNewBoard = document.getElementById('start-new-board')
+const form = document.getElementById('create-new-board-form')
+
+createNewBoard.addEventListener('click', () => {
+  form.classList.remove("none")
+})
+
+const closeBtn = document.getElementById('close')
+closeBtn.addEventListener('click', () => {
+  form.classList.add('none')
+})
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault()
+
+  const title = document.getElementById("title")
+  const description = document.getElementById("description")
+
+  const response = await fetch('http://localhost:8080/board/create', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('jwt_token')}`
+    },
+    body: JSON.stringify({
+      'title': title.value,
+      'description': description.value,
+    })
+  })
+
+  const data = await response.json()
+  
+  if(data.statusCode === 400) {
+    title.style.border = "2px dotted red"
+    description.style.border = "2px dotted red"
+  }
+
+  if(data.statusCode === 401) {
+    location.replace('http://127.0.0.1:5500/index.html')
+  }
+})
