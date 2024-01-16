1a7678c1af7015dc0298ee5cfa30528031962ad9
fiamon <juliogustavofi@gmail.com>
2024-01-15 22:05:56 -0300

    feat: list, delete, create tasks

--- scripts/boards/createBoard.js
+++ scripts/boards/createBoard.js
@@ -2,9 +2,10 @@ const createNewBoard = document.getElementById('start-new-board')
 const form = document.getElementById('create-new-board-form')
 const newListBtnHeader = document.getElementById('new-list-header')
 const listsDiv = document.getElementById('lists')
+const logoutBtn = document.getElementById('logout')
 
-document.addEventListener('DOMContentLoaded', async () => {
-  await fetchBoards()
+logoutBtn.addEventListener('click', () => {
+  location.replace('http://127.0.0.1:5500/index.html')
 })
 createNewBoard.addEventListener('click', () => {
   form.classList.remove("none")
@@ -15,6 +16,13 @@ newListBtnHeader.addEventListener('click', () => {
 const closeBtn = document.getElementById('close')
 closeBtn.addEventListener('click', () => {
   form.classList.add('none')
+})
+const submitBtn = document.getElementById('submit-btn')
+submitBtn.addEventListener('click', () => {
+  form.classList.add('none')
+})
+document.addEventListener('DOMContentLoaded', async () => {
+await fetchBoards()
 })
 
 form.addEventListener('submit', async (e) => {
@@ -46,10 +54,6 @@ form.addEventListener('submit', async (e) => {
     return location.replace('http://127.0.0.1:5500/index.html')
   }
   
-  const submitBtn = document.getElementById('submit-btn')
-  submitBtn.addEventListener('click', () => {
-    form.classList.add('none')
-  })
 })
 
 async function fetchBoards(){
@@ -74,9 +78,110 @@ async function fetchBoards(){
       for(let j = 0; j<boards.length; j++) {
         const element = document.createElement('div')
         element.classList.add('list')
+        element.id = boards[j].id
         element.innerHTML = `<span>${boards[j].title}</span><p>${boards[j].description}</p>`
+        element.onclick= function() {
+          renderTodos(this.id)
+          boardId = this.id
+        }
         listsDiv.appendChild(element)
       }
     }
     return data
+}
+
+async function renderTodos(id) {
+  const div = document.getElementById(id)
+  const createNewBoard = document.getElementById('new-board')
+  const board = document.getElementById('board')
+
+  createNewBoard.classList.add('none')
+  div.style.backgroundColor = 'rgba(128, 128, 128, 0.30)'
+  div.style.borderRight = '3px solid #5AC7AA'
+
+  const response = await fetch('http://localhost:8080/board/' + id, {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('jwt_token')}`
+    },
+  })
+  const boardWithTasks = await response.json()
+
+  const title = document.getElementById('title-board')
+  title.innerText = boardWithTasks.title
+
+  const deleteBoard = document.getElementById('delete')
+  deleteBoard.addEventListener('click', async () => {
+    await fetch('http://localhost:8080/board/' + id, {
+      method: 'DELETE',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('jwt_token')}`
+      },
+    })
+    div.remove()
+    createNewBoard.classList.remove('none')
+  })
+  const formNewTask = document.getElementById('form-new-task')
+  formNewTask.addEventListener('submit', async e => {
+    e.preventDefault()
+    submitNewTaskFrom(id)
+  })
+
+  board.classList.remove('none')
+}
+
+const newTaskBtn = document.getElementById("new-task")
+newTaskBtn.addEventListener('click', () => {
+  const newTaskForm = document.getElementById('new-task-form')
+  newTaskForm.classList.remove('none')
+
+  const closeNewTaskFormBtn = document.getElementById('close-new-task-form')
+  closeNewTaskFormBtn.addEventListener('click', () => {
+    newTaskForm.classList.add('none')
+  })
+})
+
+async function submitNewTaskFrom(id) {
+    const response = await fetch(`http://localhost:8080/board/${id}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('jwt_token')}`
+      }
+    })
+    const data = await response.json()
+  
+    const title = document.getElementById('title-todo').value
+    const status = document.getElementById('select-progress').value
+    const description = document.getElementById('description-todo').value
+    
+    const membersField = document.getElementById('member')
+    for(val of data.users) {
+      const element = document.createElement('option')
+      element.value = val.id
+      const firstName = val.full_name.split(' ')
+      element.innerText = firstName[0]
+      membersField.appendChild(element)
+    }
+
+    fetch('http://localhost:8080/board/task', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('jwt_token')}`
+      },
+      body: JSON.stringify({
+        title,
+        description,
+        status,
+        assigned_user_id: membersField.value,
+        assigned_board_id: id
+      })
+    })
 }
