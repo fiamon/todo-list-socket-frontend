28df69781a02ad5cbb0185ef9fefa0690baf5ec4
fiamon <juliogustavofi@gmail.com>
2024-01-16 20:43:34 -0300

    refactor: fetch boards

--- scripts/boards/createBoard.js
+++ scripts/boards/createBoard.js
@@ -1,5 +1,4 @@
 const form = document.getElementById('create-new-board-form')
-const listsDiv = document.getElementById('lists')
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault()
@@ -29,63 +28,4 @@ form.addEventListener('submit', async (e) => {
   if (data.statusCode === 401) {
     return location.replace('http://127.0.0.1:5500/index.html')
   }
-
-})
-
-async function renderTodos(id) {
-  const div = document.getElementById(id)
-  const createNewBoard = document.getElementById('new-board')
-  const board = document.getElementById('board')
-  createNewBoard.classList.add('none')
-  div.style.backgroundColor = 'rgba(128, 128, 128, 0.30)'
-  div.style.borderRight = '3px solid #5AC7AA'
-
-  const response = await fetch('http://localhost:8080/board/' + id, {
-    method: 'GET',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${localStorage.getItem('jwt_token')}`
-    },
-  })
-  const boardWithTasks = await response.json()
-
-  const title = document.getElementById('title-board')
-  title.innerText = boardWithTasks.title
-  
-  const membersField = document.getElementById('member')
-  for (val of data.users) {
-    const element = document.createElement('option')
-    element.value = val.id
-    const firstName = val.full_name.split(' ')
-    element.innerText = firstName[0]
-    membersField.appendChild(element)
-  }
-
-  const deleteBoard = document.getElementById('delete')
-  deleteBoard.addEventListener('click', async () => {
-    await deleteBoard(id)
-    div.remove()
-    createNewBoard.classList.remove('none')
-  })
-
-
-  const formNewTask = document.getElementById('form-new-task')
-  formNewTask.addEventListener('submit', async e => {
-    e.preventDefault()
-    submitNewTaskForm(id)
-  })
-
-  board.classList.remove('none')
-}
-
-const newTaskBtn = document.getElementById("new-task")
-newTaskBtn.addEventListener('click', async () => {
-  const newTaskForm = document.getElementById('new-task-form')
-  newTaskForm.classList.remove('none')
-
-  const closeNewTaskFormBtn = document.getElementById('close-new-task-form')
-  closeNewTaskFormBtn.addEventListener('click', () => {
-    newTaskForm.classList.add('none')
-  })
 })
---
+++ scripts/boards/fetchBoards.js
@@ -0,0 +1,33 @@
+const listsDiv = document.getElementById('lists')
+async function fetchBoards() {
+  const response = await fetch('http://localhost:8080/board', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${localStorage.getItem('jwt_token')}`
+    }
+  })
+
+  if (!response.ok) {
+    location.replace('http://127.0.0.1:5500/index.html')
+  }
+
+  let data = [await response.json()]
+
+  for (let i = 0; i < data.length; i++) {
+    const boards = data[0].boards
+
+    for (let j = 0; j < boards.length; j++) {
+      const element = document.createElement('div')
+      element.classList.add('list')
+      element.id = boards[j].id
+      element.innerHTML = `<span>${boards[j].title}</span><p>${boards[j].description}</p>`
+      element.onclick = function () {
+        renderTodos(this.id)
+      }
+      listsDiv.appendChild(element)
+    }
+  }
+  return data
+}
